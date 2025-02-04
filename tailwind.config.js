a3f161bd0252e69df738bc44e2be37db42f1511b
17Mireia18 <miralipalco@gmail.com>
2025-02-04 13:06:34 +0100

    h

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
-/*
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
- @type {import('tailwindcss').Config} 
+/** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/.php',
-        './resources/views//.blade.php',
-    ],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',  // Corregido para incluir todos los archivos .blade.php en la carpeta de views
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [forms],
 };
-*/
